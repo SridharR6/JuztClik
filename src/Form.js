028d8a063b598950b71e09873166b02cfcce94e1
SridharR6 <sridhar05598@gmail.com>
2022-11-14 21:40:49 +0530

    refered

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import {Box,Button,Typography,TextField, inputClasses} from "@mui/material";
 import { useState } from "react";
+import axios from "axios";
 
 function Form(){
   const [isSignUp,setIsSignUp] = useState(false);
@@ -11,15 +12,26 @@ function Form(){
       mob:"",
     }
   )
-  const handleChange = (e)=>{
-    setInput((prevState)=>({
-      ...prevState,
-      [e.target.name]:e.target.value
-    }))
+  function handleChange(event){
+    const {name,value} = event.target;
+    setInput(prevInput =>{
+      return{
+        ...prevInput,
+        [name]:value
+      }
+    })
   }
   const handleSubmit = (e)=>{
     e.preventDefault();
-    console.log(input);
+
+    const newUser = {
+      fname:input.fname,
+      email:input.email,
+      pwd:input.pwd,
+      mob:input.mob
+    }
+
+    axios.post("http://localhost:3001/userlogin",newUser);
   }
   const resetState = ()=>{
     setIsSignUp(!isSignUp);   
@@ -47,22 +59,22 @@ function Form(){
         >
           <Typography variant = "h2" padding = {3} textAlign = "center">{isSignUp ? "SignUp":"Login"}</Typography>
           {isSignUp &&          
-              <TextField value = {input.name} name = "fname" margin = "normal" type = {'text'} variant = "outlined" onChange = {handleChange} placeholder = "Name" required/>
+              <TextField value = {input.fname} name = "fname" margin = "normal" type = {'text'} variant = "outlined" onChange = {handleChange} placeholder = "Name" required/>
           }
          
-          <TextField value = {input.name} name  = "email" margin = "normal" type = {'email'} variant = "outlined" placeholder = "Email" onChange = {handleChange} required/>
+          <TextField value = {input.email} name  = "email" margin = "normal" type = {'email'} variant = "outlined" placeholder = "Email" onChange = {handleChange} required/>
           
           {isSignUp &&          
-                  <TextField value = {input.name} name = "mob" placeholder = "Phone Number" margin = "normal" type = {'number'} variant = "outlined" onChange = {handleChange} required/>
+                  <TextField value = {input.mob} name = "mob" placeholder = "Phone Number" margin = "normal" type = {'number'} variant = "outlined" onChange = {handleChange} required/>
           }
 
-          <TextField name  = "pwd" value = {input.name} margin = "normal" type = {'password'} variant = "outlined" placeholder = "Password" onChange = {handleChange} required/>
+          <TextField name  = "pwd" value = {input.pwd} margin = "normal" type = {'password'} variant = "outlined" placeholder = "Password" onChange = {handleChange} required/>
           {isSignUp &&          
                   <br></br>
           }
           
-          <Button type = "submit" sx = {{marginTop:3, borderRadius:3}} variant = "contained" color = "secondary"> {isSignUp ? "SignUp":"Login"}</Button>
-          <Button onClick = {()=>setIsSignUp(!isSignUp)} type = "reset" sx = {{marginTop:3, borderRadius:3}} variant = "outlined" color = "secondary">Change To {isSignUp ? "Login":"SignUp"}</Button>
+          <Button onClick={handleSubmit} sx = {{marginTop:3, borderRadius:3}} variant = "contained" color = "secondary"> {isSignUp ? "SignUp":"Login"}</Button>
+          <Button onClick = {resetState} sx = {{marginTop:3, borderRadius:3}} variant = "outlined" color = "secondary">Change To {isSignUp ? "Login":"SignUp"}</Button>
 
 
         </Box>
